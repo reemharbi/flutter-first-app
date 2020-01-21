f66ae6916d1a176a4f1c3ce9ae0159fdaa25d866
reemharbi <reemharbi.3@gmail.com>
2020-01-21 13:43:45 +0300

    Conditional Widget Output

--- lib/main.dart
+++ lib/main.dart
@@ -14,51 +14,70 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['red', 'blue', 'black']
+    },
+    {
+      'questionText': 'What\'s your favorite animal',
+      'answers': ['mouse', 'dog', 'cat']
+    },
+    {
+      'questionText': 'What\'s your favorite movie?',
+      'answers': ['SAW', 'titanic', 'fight club']
+    },
+    {
+      'questionText': 'What\'s your favorite city?',
+      'answers': ['Dammam', 'Jeddah', 'Riyadh']
+    },
+    {
+      'questionText': 'What\'s your favorite food?',
+      'answers': ['pizza', 'sushi', 'pasta', 'Mandi']
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('Next question');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
-      'What\'s your favorite food?',
-      'What\'s your favorite movie?',
-      'What\'s your favorite song?',
-      'What\'s your favorite city?',
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(
-              questions[_questionIndex],
-            ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-
-            // RaisedButton(
-            //   child: Text('Answer 2'),
-            //   onPressed: () => print('Answer 2 chosen!'),
-            // ),
-            // RaisedButton(
-            //   child: Text('Answer 3'),
-            //   onPressed: () {
-            //     print('Answer 3 chosen!!!');
-            //   },
-            // ),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: <Widget>[
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                  // RaisedButton(
+                  //   child: Text('Answer 2'),
+                  //   onPressed: () => print('Answer 2 chosen!'),
+                  // ),
+                  // RaisedButton(
+                  //   child: Text('Answer 3'),
+                  //   onPressed: () {
+                  //     print('Answer 3 chosen!!!');
+                  //   },
+                  // ),
+                ],
+              )
+            : Center(child: Text('Quiz is over.')),
       ),
     );
   }
